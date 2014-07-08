b10418003043fad80cfab423645e1c647ce3fca5
Enamel Systems <enamel.systems@gmail.com>
2014-07-09 01:34:29 +0900

    Merge xcode-copyright-init.sh and xcode-copyright-original-checkout.sh functionality to xcode-copyright-replace.sh.

--- xcode-copyright-replace.sh
+++ xcode-copyright-replace.sh
@@ -2,8 +2,8 @@
 ##
 ## Replace Xcode header template's copyright with ${COPYRIGHT}
 ##
-XCODE=Xcode
-XCODE=Xcode6-Beta2
+#XCODE=Xcode
+XCODE=Xcode6-Beta3
 
 ## Copyright string.
 COPYRIGHT="Copyright (c) 2014 Enamel Systems. All rights reserved."
@@ -11,8 +11,55 @@ COPYRIGHT="Copyright (c) 2014 Enamel Systems. All rights reserved."
 ## Base directory which has Xcode header template.
 BASE="/Applications/${XCODE}.app/Contents/Developer"
 
-for i in ${BASE}/Library/Xcode/Templates \
-         ${BASE}/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates
+## Template directories
+TEMPLATE="${BASE}/Library/Xcode/Templates \
+          ${BASE}/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates"
+
+## Directory check.
+for i in ${TEMPLATE}
+do
+if [ ! -d $i ];then
+  echo "$i does not exists."
+  echo "Please check XCODE variable in the script."
+  exit 1
+fi
+done
+
+## Initialization.
+if [ x"$1" = x"-i" ];then
+  for i in ${TEMPLATE}
+  do
+    echo "git init ${i}..."
+    cd ${i}
+    if [ ! -d ./.git ];then
+      sudo git init
+      sudo git add .
+      sudo git commit -m 'Initial commmit'
+    fi
+  done
+  exit 0
+fi
+
+## Check that directory has .git.
+for i in ${TEMPLATE}
+do
+if [ ! -d $i/.git ];then
+  echo "git repository has not yet configured: $i."
+  echo "Please execute $0 -i" first.
+  exit 2
+fi
+done
+
+## Ok, restore the original file.
+for i in ${TEMPLATE}
+do
+echo "Checking out ${i}..."
+cd ${i}
+sudo git checkout .
+done
+
+## Now replace the Copyright description.
+for i in ${TEMPLATE}
 do
 echo "Replacing copyright ${i}..."
 cd ${i}
